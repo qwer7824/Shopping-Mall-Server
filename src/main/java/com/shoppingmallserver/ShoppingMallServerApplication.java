918500f118089b65a4180fc19a3d8a1eaee0ffdc
dev <sh7824@naver.com>
2023-03-27 22:57:41 +0900

    Setting

---
+++ src/main/java/com/shoppingmallserver/ShoppingMallServerApplication.java
@@ -0,0 +1,13 @@
+package com.shoppingmallserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMallServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingMallServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shoppingmallserver/ShoppingMallServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingmallserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMallServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
