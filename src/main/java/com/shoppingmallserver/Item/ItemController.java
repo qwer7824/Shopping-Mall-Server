e73e7e9580016649e5917a5b4b53ab5adaf05988
KSH <sh7824@naver.com>
2023-04-14 01:39:14 +0900

    상세페이지 api

--- src/main/java/com/shoppingmallserver/Item/ItemController.java
+++ src/main/java/com/shoppingmallserver/Item/ItemController.java
@@ -5,10 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,13 +17,17 @@ public class ItemController {
     private final ItemService itemService;
 
     @GetMapping("/items")
-    public List<Item> getItems() {
-        List<Item> items = itemRepository.findAll();
-        return items;
+    public ResponseEntity<?> getitems() {
+        return ResponseEntity.ok().body(itemService.allItemView());
     }
     @PostMapping(value = "/additem")
     public ResponseEntity<String> additem(@RequestBody ItemRequest request) throws Exception {
         return new ResponseEntity<>(itemService.additem(request), HttpStatus.OK);
     }
 
+    @GetMapping("/item/{id}")
+    public ResponseEntity<?> getProductDetails(@PathVariable Long id) {
+
+        return ResponseEntity.ok().body(itemService.getProductDetails(id));
+    }
 }
--- src/main/java/com/shoppingmallserver/Item/ItemService.java
+++ src/main/java/com/shoppingmallserver/Item/ItemService.java
@@ -4,13 +4,28 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class ItemService {
 
     private final ItemRepository itemRepository;
+    public Item itemView(Long id) {
+        return itemRepository.findById(id).get();
+    }
 
+
+    public List<Item> allItemView() {
+        return itemRepository.findAll();
+    }
+
+    public Item getProductDetails(Long id) {
+
+        return itemRepository.findById(id).get();
+    }
 
     public String additem(ItemRequest request) {
         itemRepository.save(Item.builder()
