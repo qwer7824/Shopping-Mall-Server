3927bcef3d1f4cae407ac4dbb94b689211fd07a8
qwer7824 <sh7824@naver.com>
2023-08-14 17:10:38 +0900

    리펙토링
    modelmapper 및 ResponseDTO 추가 및 설정

---
+++ src/main/java/com/shoppingmallserver/Exception/ResultType.java
@@ -0,0 +1,16 @@
+package com.shoppingmallserver.Exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ResultType {
+
+  SUCCESS("0000", "success"),
+  NOT_EXIST("4001", "file not exist"),
+  SYSTEM_ERROR("9000", "system error");
+
+  private final String code;
+  private final String desc;
+}
